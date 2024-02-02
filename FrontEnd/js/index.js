f098115dfd9a75d48b38c56ea328d27bd9090f8f
PPlonq <115898173+PPlonq@users.noreply.github.com>
2024-02-02 16:29:56 +0100

    feat(works)/display-fetched-images-and-text

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,20 +1,34 @@
-const gallery = document.querySelector(".gallery")
-console.log ("gallery elements:", gallery)
-
-gallery.innerHTML = "Hello, World!";
-
 fetch("http://localhost:5678/api/works")
   .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    return response.json(); // Convertir la réponse en JSON
+    return response.json();
   })
   .then((works) => {
-    console.log(works); 
-    // Faites ici ce que vous voulez faire avec les données
+    const galleryDiv = document.querySelector('.gallery');
+
+    works.forEach((project) => {
+      // Créez un élément figure pour chaque projet
+      const figure = document.createElement('figure');
+
+      // Créez un élément img pour l'image du projet
+      const img = document.createElement('img');
+      img.src = project.imageUrl; 
+      img.alt = project.title;
+
+      // Créez un élément figcaption pour le titre du projet
+      const figcaption = document.createElement('figcaption');
+      figcaption.textContent = project.title;
+
+      // Ajoutez l'image et le titre à la figure
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+
+      // Ajoutez la figure à la galerie
+      galleryDiv.appendChild(figure);
+    });
   })
   .catch((error) => {
     console.error("Fetch error:", error);
   });
-
